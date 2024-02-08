57acc5d3cedabd9fa578d25437b34a0bf57be856
Persune <gumball2415@gmail.com>
2024-02-08 10:39:15 +0800

    Procedurally generate shadow mask textures

--- render_renders.py
+++ render_renders.py
@@ -6,28 +6,63 @@ print(argv[0])
 print(argv[1])
 print(argv[2])
 print(argv[3])
+print(argv[4])
 
-pvm_shadowmask = ["//texture/shadowmask_inline-unlit_10x10_R.png","//texture/shadowmask_inline-unlit_10x10_G.png","//texture/shadowmask_inline-unlit_10x10_B.png"]
-crt_shadowmask = ["//texture/shadowmask_inline_10x10_R.png","//texture/shadowmask_inline_10x10_G.png","//texture/shadowmask_inline_10x10_B.png"]
-tvl = float(argv[2])
+dotpitch = float(argv[2])
+world_strength = float(argv[4])
 
 import bpy
 bpy.context.scene.camera = bpy.data.objects[argv[0]]
-bpy.data.materials["TV-CRT"].node_tree.nodes['Source Image R'].image.filepath = argv[1]
-bpy.data.materials["TV-CRT"].node_tree.nodes['Source Image G'].image.filepath = argv[1]
-bpy.data.materials["TV-CRT"].node_tree.nodes['Source Image B'].image.filepath = argv[1]
-bpy.data.materials["TV-CRT"].node_tree.nodes['Source Image B'].image.filepath = argv[1]
 
-bpy.data.materials["TV-CRT"].node_tree.nodes['TVL'].outputs[0].default_value = tvl
+tv_crt = bpy.data.materials["TV-CRT"].node_tree
+
+tv_crt.nodes['Source Image R'].image.filepath = argv[1]
+tv_crt.nodes['Source Image G'].image.filepath = argv[1]
+tv_crt.nodes['Source Image B'].image.filepath = argv[1]
+
+tv_crt.nodes['Dot Pitch'].outputs[0].default_value = dotpitch
+
+bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = world_strength
+
+
+vector = tv_crt.nodes["Texture Coordinate"]
+
+node_r_location = tv_crt.nodes["Combine XYZ.003"]
+node_g_location = tv_crt.nodes["Combine XYZ.004"]
+node_b_location = tv_crt.nodes["Combine XYZ.005"]
+
+node_b_scale = tv_crt.nodes["Vector Math.003"]
+
+node_r_out = tv_crt.nodes["Vector Math.004"]
+node_g_out = tv_crt.nodes["Vector Math.005"]
+node_b_out = tv_crt.nodes["Vector Math.006"]
+
+node_r_phosphor_light = tv_crt.nodes["R Phosphor light"]
+node_g_phosphor_light = tv_crt.nodes["G Phosphor light"]
+node_b_phosphor_light = tv_crt.nodes["B Phosphor light"]
 
 if argv[3] == "PVM":
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask R'].image.filepath = pvm_shadowmask[0]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask G'].image.filepath = pvm_shadowmask[1]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask B'].image.filepath = pvm_shadowmask[2]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['Scanline gamma'].outputs[0].default_value = 8.0
+  node_r_phosphor_light.node_tree = bpy.data.node_groups.get("Phosphor Stripe")
+  node_g_phosphor_light.node_tree = bpy.data.node_groups.get("Phosphor Stripe")
+  node_b_phosphor_light.node_tree = bpy.data.node_groups.get("Phosphor Stripe")
+  tv_crt.nodes['Scanline gamma'].outputs[0].default_value = 6
 
 if argv[3] == "CRT":
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask R'].image.filepath = crt_shadowmask[0]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask G'].image.filepath = crt_shadowmask[1]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['phosphor shadowmask B'].image.filepath = crt_shadowmask[2]
-  bpy.data.materials["TV-CRT"].node_tree.nodes['Scanline gamma'].outputs[0].default_value = 2.0
+  node_r_phosphor_light.node_tree = bpy.data.node_groups.get("Shadow Mask")
+  node_g_phosphor_light.node_tree = bpy.data.node_groups.get("Shadow Mask")
+  node_b_phosphor_light.node_tree = bpy.data.node_groups.get("Shadow Mask")
+  tv_crt.nodes['Scanline gamma'].outputs[0].default_value = 2.0
+
+# relink nodes
+tv_crt.links.new(node_r_phosphor_light.outputs[0], node_r_out.inputs[0])
+tv_crt.links.new(node_g_phosphor_light.outputs[0], node_g_out.inputs[0])
+tv_crt.links.new(node_b_phosphor_light.outputs[0], node_b_out.inputs[0])
+tv_crt.links.new(vector.outputs[3], node_r_phosphor_light.inputs[0])
+tv_crt.links.new(vector.outputs[3], node_g_phosphor_light.inputs[0])
+tv_crt.links.new(vector.outputs[3], node_b_phosphor_light.inputs[0])
+tv_crt.links.new(node_r_location.outputs[0], node_r_phosphor_light.inputs[1])
+tv_crt.links.new(node_g_location.outputs[0], node_g_phosphor_light.inputs[1])
+tv_crt.links.new(node_b_location.outputs[0], node_b_phosphor_light.inputs[1])
+tv_crt.links.new(node_b_scale.outputs[0], node_r_phosphor_light.inputs[2])
+tv_crt.links.new(node_b_scale.outputs[0], node_g_phosphor_light.inputs[2])
+tv_crt.links.new(node_b_scale.outputs[0], node_b_phosphor_light.inputs[2])
--- render_renders_eevee.bat
+++ render_renders_eevee.bat
@@ -1,11 +1,11 @@
 @echo off
 
 REM issues with running in the background
-del renders\yoeyrom1_0.png
-blender subpixel_example.blend -o //renders/yoeyrom1_#.png -F PNG -E BLENDER_EEVEE -t 0 --python render_renders.py -f 0 -- "Camera 7" "//texture/yoeyrom_crt_000.png" 900 PVM
+REM del renders\yoeyrom1_0.png
+blender subpixel_example.blend -o //renders/yoeyrom1_#.png -F PNG -E BLENDER_EEVEE -t 0 --python render_renders.py -f 0 -- "Camera 7" "//texture/yoeyrom_crt_000.png" 900 PVM 0
 
-del renders\yoeyrom2_0.png
-blender subpixel_example.blend -o //renders/yoeyrom2_#.png -F PNG -E BLENDER_EEVEE -t 0 --python render_renders.py -f 0 -- "Camera 8" "//texture/yoeyrom_crt_000.png" 900 PVM
+REM del renders\yoeyrom2_0.png
+blender subpixel_example.blend -o //renders/yoeyrom2_#.png -F PNG -E BLENDER_EEVEE -t 0 --python render_renders.py -f 0 -- "Camera 8" "//texture/yoeyrom_crt_000.png" 900 PVM 0
 
 rem Camera 7: yoeyrom1.png
 rem Camera 8: yoeyrom2.png
